e6bd433cd881d94c13036f57e386ad07790e9885
Filip Egge <filip.egge@gmail.com>
2016-04-05 21:51:27 +0200

    Add possibility of having sub menus in pages menu

--- app/assets/javascripts/pages/pages.js
+++ app/assets/javascripts/pages/pages.js
@@ -11,12 +11,18 @@ $(function() {
   $('.menu-content a').each(function() {
     if(page_name(window.location.pathname) == page_name($(this).attr('href'))) {
         $(this).addClass('current');
-        $(this).parents('ul').addClass('active');
+        $(this).parents('ul, li').addClass('active');
         $(this).parents('ul').prev('h1').addClass('active');
     }
   });
   $('.menu-content h1').click(function() {
     $(this).toggleClass('active');
     $(this).next('ul').toggleClass('active');
+  });
+  $('.menu-content li > ul').parent().addClass('submenu');
+
+  $('.menu-content li.submenu').click(function() {
+    $(this).toggleClass('active');
+    $(this).children('ul').toggleClass('active');
   });
 });
