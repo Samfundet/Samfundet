941f16277b41451cb62895654bee5975e8d3ecda
Filip Egge <filip.egge@gmail.com>
2016-08-26 20:33:37 +0200

    Make rubocop pass without making big changes

--- app/controllers/admissions_admin/applicants_controller.rb
+++ app/controllers/admissions_admin/applicants_controller.rb
@@ -3,7 +3,8 @@ class AdmissionsAdmin::ApplicantsController < ApplicationController
     @admission = Admission.find(params[:admission_id])
     if @admission.admin_priority_deadline > Time.current
       return @applicants = []
+    else
+      @applicants = Applicant.interested_other_positions(@admission)
     end
-    @applicants = Applicant.interested_other_positions(@admission)
   end
 end
