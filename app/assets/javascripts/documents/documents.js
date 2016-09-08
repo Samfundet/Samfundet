91562962e3e5df4d2cceb333d940cf561b61782b
stianste <stianste@stud.ntnu.no>
2016-09-08 21:27:47 +0200

    Make top year and month active on load

--- app/assets/javascripts/documents/documents.js
+++ app/assets/javascripts/documents/documents.js
@@ -1,11 +1,15 @@
 function add_class_to_document_submenus() {
-  $('.documents_list li > ul').each(function() {
+  $('.documents_list li > ul').each(function(index) {
     $(this).parent().addClass('submenu');
+    if (index < 2){
+      //Set the first year and most recent month active on load.
+      $(this).parent().addClass('active');
+      $(this).addClass('active');
+    }
   });
 }
 function add_click_listener_to_document_submenus() {
   $('.documents_list li.submenu > strong').click(function(event) {
-    event.stopPropagation();
     submenu = $(this).parent();
     if (submenu.hasClass('active')) {
       submenu.find('li.active').removeClass('active');
@@ -18,7 +22,6 @@ function add_click_listener_to_document_submenus() {
 }
 
 $(function() {
-
     set_active_menu_item();
     add_class_to_document_submenus();
     add_click_listener_to_document_submenus();
