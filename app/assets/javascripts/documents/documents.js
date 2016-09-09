c66563076cc2f6c59f475255687348c360b2f1d7
Alf Alexander <alf1337@gmail.com>
2016-09-09 03:48:27 +0200

    Rename submenus to sublists and change document_root to documents_list_root

--- app/assets/javascripts/documents/documents.js
+++ app/assets/javascripts/documents/documents.js
@@ -1,23 +1,23 @@
-function add_class_to_document_submenus() {
+function add_class_to_document_sublists() {
   $('.documents_list li > ul').each(function() {
     $(this).parent().addClass('sublist');
   });
 }
-function add_click_listener_to_document_submenus() {
+function add_click_listener_to_document_sublists() {
   $('.documents_list li.sublist > strong').click(function(event) {
-    submenu = $(this).parent();
-    if (submenu.hasClass('active')) {
-      submenu.find('li.active').removeClass('active');
-      submenu.find('ul.active').removeClass('active');
+    sublist = $(this).parent();
+    if (sublist.hasClass('active')) {
+      sublist.find('li.active').removeClass('active');
+      sublist.find('ul.active').removeClass('active');
     } else  {
-      submenu.children('ul').toggleClass('active');
+      sublist.children('ul').toggleClass('active');
     }
-    submenu.toggleClass('active');
+    sublist.toggleClass('active');
   });
 }
 function open_most_recent() {
-  $('ul.document_root > li:first-child').each(function() {
-    // Set list inside of first list-element as active
+  $('ul.documents_list_root > li:first-child').each(function() {
+    // Set ul inside of first li-element as active
     $(this).children('ul').first().addClass('active');
 
     // Find all ul children (1 or more levels down) from the first li element, which of the last is the one we want to to set as active
@@ -26,8 +26,8 @@ function open_most_recent() {
 }
 
 $(function() {
-    add_class_to_document_submenus();
-    add_click_listener_to_document_submenus();
-    $('.documents_list > ul').addClass('document_root')
+    add_class_to_document_sublists();
+    add_click_listener_to_document_sublists();
+    $('.documents_list > ul').addClass('documents_list_root')
     open_most_recent();
 });
