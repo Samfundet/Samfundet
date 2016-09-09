187e12fea77778ea6d710216c27c7ec36404de6e
Alf Alexander <alf1337@gmail.com>
2016-09-09 03:26:25 +0200

    Open the most recent month with documents in the documents list

--- app/assets/javascripts/documents/documents.js
+++ app/assets/javascripts/documents/documents.js
@@ -1,10 +1,10 @@
 function add_class_to_document_submenus() {
   $('.documents_list li > ul').each(function() {
-    $(this).parent().addClass('submenu');
+    $(this).parent().addClass('sublist');
   });
 }
 function add_click_listener_to_document_submenus() {
-  $('.documents_list li.submenu > strong').click(function(event) {
+  $('.documents_list li.sublist > strong').click(function(event) {
     submenu = $(this).parent();
     if (submenu.hasClass('active')) {
       submenu.find('li.active').removeClass('active');
@@ -16,20 +16,18 @@ function add_click_listener_to_document_submenus() {
   });
 }
 function open_most_recent() {
-  $('.documents_list').each(function() {
-    $(this).find('li > ul').each(function(index) {
-      if (index < 2){
-        //Set the first year and most recent month active on load.
-        $(this).parent().addClass('active');
-        $(this).addClass('active');
-      }
-    });
+  $('ul.document_root > li:first-child').each(function() {
+    // Set list inside of first list-element as active
+    $(this).children('ul').first().addClass('active');
+
+    // Find all ul children (1 or more levels down) from the first li element, which of the last is the one we want to to set as active
+    $(this).find('ul').last().addClass('active');
   });
 }
 
 $(function() {
-    open_most_recent();
     add_class_to_document_submenus();
     add_click_listener_to_document_submenus();
-    $('.documents_list > ul').addClass('menu_root')
+    $('.documents_list > ul').addClass('document_root')
+    open_most_recent();
 });
