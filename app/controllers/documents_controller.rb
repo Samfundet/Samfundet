93d2926709a4aa9037249b43ae206846b74aa29f
Filip Egge <filip.egge@gmail.com>
2016-09-26 19:17:57 +0200

    Add strong params for documents

--- app/controllers/documents_controller.rb
+++ app/controllers/documents_controller.rb
@@ -18,7 +18,7 @@ class DocumentsController < ApplicationController
   end
 
   def create
-    @document = Document.create(params[:document])
+    @document = Document.new(document_param)
     @document.uploader_id = current_user.id
     if @document.save
       flash[:success] = t('documents.create_success')
@@ -37,5 +37,11 @@ class DocumentsController < ApplicationController
   end
 
   def admin_applet
+  end
+
+  private
+
+  def document_param
+    params.require(:document).permit(:title, :category_id, :file, :publication_date)
   end
 end
