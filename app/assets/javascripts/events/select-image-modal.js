cf50488109eaa2c97f6e7c07c0999c910941b23b
Filip Egge <filip.egge@gmail.com>
2016-09-26 20:35:00 +0200

    Add jquery-turbolinks gem

--- app/assets/javascripts/events/select-image-modal.js
+++ app/assets/javascripts/events/select-image-modal.js
@@ -1,26 +1,24 @@
-$(function () {
-  $(document).on('click', '.modal a.image', function (e) {
-    e.preventDefault();
+$(document).on('click', '.modal a.image', function (e) {
+  e.preventDefault();
 
-    var selected_image_id = $(this).data('image');
-    var selected_image_url = $(this).data('url')
+  var selected_image_id = $(this).data('image');
+  var selected_image_url = $(this).data('url')
 
-    $(".image_id_input").val(selected_image_id);
-    $('.image-preview').attr('src', selected_image_url);
+  $(".image_id_input").val(selected_image_id);
+  $('.image-preview').attr('src', selected_image_url);
 
-    $.modal.close();
-  });
+  $.modal.close();
+});
 
-  $(document).on('click', '.modal .pagination a', function(e) {
-    e.preventDefault();
+$(document).on('click', '.modal .pagination a', function(e) {
+  e.preventDefault();
 
-    $(".modal").load(this.href).open();
-  });
+  $(".modal").load(this.href).open();
+});
 
-  $('.choose-image-link').on('click', function(e) {
-    e.preventDefault();
-    $(this).modal();
+$('.choose-image-link').on('click', function(e) {
+  e.preventDefault();
+  $(this).modal();
 
-    $("html, body").animate({ scrollTop: 0 }, "slow");
-  });
+  $("html, body").animate({ scrollTop: 0 }, "slow");
 });
