0c8766300cb92e727f0a3005978e8a4c567ed5d7
stianste <stianste@stud.ntnu.no>
2016-11-07 18:38:43 +0100

    Satisify travis

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -11,13 +11,13 @@ class Campus < ActiveRecord::Base
   end
 
   def number_of_applicants_current_admission
-    #Find all job applications of the current admission, get the unique user and group by campus.
+    # Find all job applications of the current admission, get the unique user and group by campus.
     campus_hash = Admission.current.first.job_applications.map(&:applicant).uniq.map(&:campus).group_by(&:name)
 
-    #Count how many applicants belong to this campus.
-    #If the list is Nil then there are no applicants from this campus during the current admission.
+    # Count how many applicants belong to this campus.
+    # If the list is Nil then there are no applicants from this campus during the current admission.
     begin
-      campus_hash[self.name].length
+      campus_hash[name].length
     rescue
       0
     end
