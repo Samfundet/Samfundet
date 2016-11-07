f5702ab11e306e5cba9a6040bc8be108c29151a1
stianste <stianste@stud.ntnu.no>
2016-11-07 22:36:00 +0100

    Add spaces to please travis

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -14,7 +14,7 @@ class Campus < ActiveRecord::Base
     # Find all job applications of the current admission, get the unique user_id and group by campus_id.
     applicant_ids = Admission.current.first.job_applications.pluck(:applicant_id)
     campus_count = {}
-    Applicant.where(id:applicant_ids).pluck(:campus_id).group_by{|i| i}.each{ |k,v| campus_count[k] = v.length}
+    Applicant.where(id: applicant_ids).pluck(:campus_id).group_by { |i| i }.each { |k, v| campus_count[k] = v.length }
     campus_count.default = 0
     campus_count
   end
