5af561a1104c1ff1379b11dc65234ce0874f910c
Stian Steinbakken <stiansteinbakken94@gmail.com>
2017-01-03 17:26:59 +0100

    Handle no current admission at campus overview (#184)
    * Fix case when there is no current admission
    
    * Exclude campus_controller from rubocop ClassAndModuleChildren test

--- app/models/campus.rb
+++ app/models/campus.rb
@@ -12,10 +12,15 @@ class Campus < ActiveRecord::Base
 
   def self.number_of_applicants_current_admission
     # Find all job applications of the current admission, get the unique user_id and group by campus_id.
-    applicant_ids = Admission.current.first.job_applications.pluck(:applicant_id)
-    campus_count = {}
-    Applicant.where(id: applicant_ids).pluck(:campus_id).group_by { |i| i }.each { |k, v| campus_count[k] = v.length }
-    campus_count.default = 0
-    campus_count
+    current = Admission.current
+    if current.nil?
+      0
+    else
+      applicant_ids = current.first.job_applications.pluck(:applicant_id)
+      campus_count = {}
+      Applicant.where(id: applicant_ids).pluck(:campus_id).group_by { |i| i }.each { |k, v| campus_count[k] = v.length }
+      campus_count.default = 0
+      campus_count
+    end
   end
 end
