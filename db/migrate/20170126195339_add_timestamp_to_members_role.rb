19cd454b6784841685d281148f0175f6c1ab4286
stianste <stianste@stud.ntnu.no>
2017-01-30 19:16:47 +0100

    Add timestamp migration

---
+++ db/migrate/20170126195339_add_timestamp_to_members_role.rb
@@ -0,0 +1,6 @@
+class AddTimestampToMembersRole < ActiveRecord::Migration
+  def change
+    add_column :members_roles, :created_at, :datetime, :null => false, :default => DateTime.now
+    add_column :members_roles, :updated_at, :datetime, :null => false, :default => DateTime.now
+  end
+end
