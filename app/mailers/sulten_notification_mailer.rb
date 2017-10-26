9be43c926d11dc069012604ade1287f8109dd301
Tollef Jørgensen <tollefj@gmail.com>
2017-10-26 18:37:49 +0200

    Lyche back to @samfundet.no

--- app/mailers/sulten_notification_mailer.rb
+++ app/mailers/sulten_notification_mailer.rb
@@ -1,6 +1,6 @@
 class SultenNotificationMailer < ActionMailer::Base
   default from: "no-reply@samfundet.no",
-          reply_to: "lyche@uka.no"
+          reply_to: "lyche@samfundet.no"
 
   def send_reservation_email(reservation)
     @reservation = reservation
