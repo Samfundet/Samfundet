5cfc0a2e77083c7dee6a397c80bd516075fffb90
KevinKaffe <kevikris@gmail.com>
2018-02-08 19:25:41 +0100

    Added chart of applicant by campus (#292)
    * Added chart of applicant by campus
    
    * Fixed some formatting issues
    
    * Removed trailing whitespaces

--- app/controllers/admissions_admin/admissions_controller.rb
+++ app/controllers/admissions_admin/admissions_controller.rb
@@ -15,6 +15,8 @@ class AdmissionsAdmin::AdmissionsController < ApplicationController
 
   def statistics
     @admission = Admission.find(params[:id])
+    @campuses = Campus.order(:name)
+    @campus_count = Campus.number_of_applicants_given_admission(@admission)
 
     applications_count = @admission.job_applications.count
     applications_per_group = @admission.groups.map do |group|
@@ -23,7 +25,6 @@ class AdmissionsAdmin::AdmissionsController < ApplicationController
       end.sum
     end
     group_labels = @admission.groups.map(&:name)
-
     admission_start = @admission.shown_from.to_date
     admission_end = @admission.actual_application_deadline.to_date
     applications_per_day = (admission_start..admission_end).map do |day|
@@ -34,6 +35,14 @@ class AdmissionsAdmin::AdmissionsController < ApplicationController
       day.strftime("%-d.%-m")
     end
 
+    applications_per_campus = @campuses.map do |campus|
+      @campus_count[campus.id]
+    end
+    # Want both the name of the campus, and amount of applicants
+    campus_labels = @campuses.map do |campus|
+      "#{campus.name} - #{@campus_count[campus.id]}"
+    end
+
     # The Gchart methods return an external URL to an image of the chart.
     @applications_per_group_chart = Gchart.pie(
       data: applications_per_group,
@@ -41,6 +50,14 @@ class AdmissionsAdmin::AdmissionsController < ApplicationController
       labels: group_labels,
       size: '800x300',
       custom: 'chco=00FFFF,FF0000,FFFF00,0000FF', # color scale
+    )
+
+    @applications_per_campus_chart = Gchart.pie(
+      data: applications_per_campus,
+      encoding: 'text',
+      labels: campus_labels,
+      size: '800x350',
+      custom: 'chco=00FFFF,FF0000,FFFF00,0000FF'
     )
 
     @applications_per_day_chart = Gchart.bar(
--- app/models/campus.rb
+++ app/models/campus.rb
@@ -10,6 +10,20 @@ class Campus < ActiveRecord::Base
     applicants.count
   end
 
+  def self.number_of_applicants_given_admission(admission)
+    # Find all job applications of the current admission, get the unique user_id and group by campus_id.
+    current = admission
+    if current.nil?
+      0
+    else
+      applicant_ids = current.job_applications.pluck(:applicant_id)
+      campus_count = {}
+      Applicant.where(id: applicant_ids).pluck(:campus_id).group_by { |i| i }.each { |k, v| campus_count[k] = v.length }
+      campus_count.default = 0
+      campus_count
+    end
+  end
+
   def self.number_of_applicants_current_admission
     # Find all job applications of the current admission, get the unique user_id and group by campus_id.
     current = Admission.current
