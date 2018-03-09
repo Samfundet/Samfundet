a1b6bde5b1bb2382a68f4d49298d492c8e89f997
Tollef Jørgensen <tollefj@gmail.com>
2018-03-09 16:28:54 +0100

    remove form

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -4,16 +4,4 @@ class ContactController < ApplicationController
   def index
     @contact_form = ContactForm.new
   end
-
-  def create
-    @contact_form = ContactForm.new(params[:contact_form])
-    if @contact_form.valid?
-      SupportRequestMailer.send_request(@contact_form).deliver
-      flash[:success] = t('contact.success_message')
-      render :success
-    else
-      flash.now[:error] = t('contact.error_message')
-      render :index
-    end
-  end
 end
