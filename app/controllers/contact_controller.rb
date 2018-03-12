9a03eef92f90f718856915692f995f4bd0b83201
Tollef Jørgensen <tollefj@gmail.com>
2018-03-12 15:08:36 +0100

    Remove email form (#306)
    * remove form
    
    * contact controller removed
    
    * remove request mailer and success page
    
    * fix Luka opening hours
    
    * clear index of contact controller

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,19 +1,5 @@
 # frozen_string_literal: true
 
 class ContactController < ApplicationController
-  def index
-    @contact_form = ContactForm.new
-  end
-
-  def create
-    @contact_form = ContactForm.new(params[:contact_form])
-    if @contact_form.valid?
-      SupportRequestMailer.send_request(@contact_form).deliver
-      flash[:success] = t('contact.success_message')
-      render :success
-    else
-      flash.now[:error] = t('contact.error_message')
-      render :index
-    end
-  end
+  def index; end
 end
