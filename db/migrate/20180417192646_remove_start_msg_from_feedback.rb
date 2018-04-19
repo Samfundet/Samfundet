30962bb349a9bbe357dad82515363799e2f0d7a0
Konstantin <konstantin@live.no>
2018-04-19 20:19:53 +0200

    Fix syntax error in migration

--- db/migrate/20180417192646_remove_start_msg_from_feedback.rb
+++ db/migrate/20180417192646_remove_start_msg_from_feedback.rb
@@ -1,5 +1,5 @@
 class RemoveStartMsgFromFeedback < ActiveRecord::Migration
   def change
-    remove_column: feedback_surveys, start_message
+    remove_column :feedback_surveys, :start_message
   end
 end
