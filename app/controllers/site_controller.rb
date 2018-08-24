7b8156e4597e3318e1d7c88b9afabb4c4e22f55c
Tollef Jørgensen <tollefj@gmail.com>
2018-08-24 04:23:20 +0200

    rework for multi-type support

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class SiteController < ApplicationController
-  after_action :check_active_notifications
+  before_action :check_active_notifications
   def index
     @todays_events = Event.today
     @upcoming_events = Event.front_page_events(11)
@@ -9,17 +9,22 @@ class SiteController < ApplicationController
   end
 
   def check_active_notifications
+    # from an dto are integers of date_type
+    # some supported (most useful) types:
+    #   %m - Month, e.g. 8 for August, 1 for January
+    #   %d - Day in month
+    #   %H - Hour of day
+    flash_in_date_range = lambda { |date_from, date_to, date_type, msg|
+      time_now = Time.now.strftime(date_type).to_i
+      return unless time_now.between?(date_from, date_to)
+      flash[:notice] = view_context.sanitize(msg)
+    }
+
     register_membership =
       t('site.notifications.membership.welcome') +
       t('site.notifications.membership.click') +
       view_context.link_to(t('site.notifications.membership.to_url'), 'https://medlem.samfundet.no') +
       t('site.notifications.membership.register')
-    flash_in_date_range 8, 9, register_membership
-  end
-
-  def flash_in_date_range(from_month, to_month, message)
-    current_month = Time.now.strftime('%m').to_i
-    return unless current_month.between?(from_month, to_month)
-    flash[:notice] = view_context.sanitize(message)
+    flash_in_date_range.call(8, 9, '%m', register_membership)
   end
 end
