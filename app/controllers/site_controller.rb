6ca6c0c1800c2e38606211eadf30fed76ef3fad1
Tollef Jørgensen <tollefj@gmail.com>
2018-08-28 18:41:48 +0200

    refactor to lambda expression

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -16,14 +16,15 @@ class SiteController < ApplicationController
     #   %H - Hour of day
     flash_in_date_range = lambda { |date_from, date_to, date_type, msg|
       time_now = Time.now.strftime(date_type).to_i
-      return unless time_now.between?(date_from, date_to)
+      return if not time_now.between?(date_from, date_to)
       flash[:notice] = view_context.sanitize(msg)
     }
 
+    # add all notifications below, call the flashes after each
     register_membership =
-      t('site.notifications.membership.welcome') +
-      t('site.notifications.membership.click') +
-      view_context.link_to(t('site.notifications.membership.to_url'), 'https://medlem.samfundet.no') +
+      t('site.notifications.membership.welcome') + ' ' +
+      t('site.notifications.membership.click') + ' ' +
+      view_context.link_to(t('site.notifications.membership.to_url'), 'https://medlem.samfundet.no') + ' ' +
       t('site.notifications.membership.register')
     flash_in_date_range.call(8, 9, '%m', register_membership)
   end
