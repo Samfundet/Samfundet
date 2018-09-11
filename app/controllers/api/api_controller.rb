e936ad16cdf12f46117b1d5a9fcd1b63e09c176e
stekern <stekern@users.noreply.github.com>
2018-09-11 21:13:38 +0200

    Update controllers

--- app/controllers/api/api_controller.rb
+++ app/controllers/api/api_controller.rb
@@ -1,4 +1,16 @@
 module Api
   class APIController < ActionController::API
+    before_action :snakecase_params!
+
+    def snakecase_params!
+      params.deep_transform_keys!(&:underscore)
+    end
+
+    def paginate(elements, page, pagesize = 10)
+      elements
+        .order(created_at: :desc)
+        .limit(pagesize)
+        .offset(page * pagesize)
+    end
   end
 end
