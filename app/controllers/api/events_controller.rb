a69a0f5c113c4f89f87630d8547d89b855d1fad9
stekern <stekern@users.noreply.github.com>
2018-09-11 21:13:38 +0200

    Update controllers

--- app/controllers/api/events_controller.rb
+++ app/controllers/api/events_controller.rb
@@ -1,8 +1,17 @@
 module Api
   class EventsController < APIController
     def index
-      @events = Event.all
-      render json: @events
+      @events = Event
+        .active
+        .published
+        .upcoming
+      page = params[:page].present? ? params[:page].to_i : 0
+      render json: paginate(@events, page)
+    end
+
+    def create
+      puts params
     end
   end
+
 end
