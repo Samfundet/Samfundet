398ba53c12832d5d21d968db537e09a2aa138c0b
stianste <stiansteinbakken94@gmail.com>
2018-10-11 21:20:24 +0200

    Add union type for organizer

--- app/graphql/samfundet_schema.rb
+++ app/graphql/samfundet_schema.rb
@@ -3,4 +3,14 @@
 class SamfundetSchema < GraphQL::Schema
   mutation(Types::MutationType)
   query(Types::QueryType)
+
+  def self.resolve_type(abstract_type, object, context)
+    if object.is_a?(Group)
+      Types::GroupType
+    elsif object.is_a?(ExternalOrganizer)
+      Types::ExternalOrganizerType
+    else
+      nil
+    end
+  end
 end
---
+++ app/graphql/types/organizer_type.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+module Types
+  class OrganizerType < BaseUnion
+    description "Organizer of an event."
+    possible_types GroupType, ExternalOrganizerType
+  end
+end
