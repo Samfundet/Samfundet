e12df66d2436d32cea33edeaffd45ab347ec1247
stekern <stekern@users.noreply.github.com>
2018-10-18 21:54:50 +0200

    Make rubocop happy

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,8 +1,9 @@
-class StaticController < ApplicationController
+# frozen_string_literal: true
 
+class StaticController < ApplicationController
   # Raise a 404 error if a template is missing
   rescue_from ActionView::MissingTemplate do
-    raise ActionController::RoutingError.new('Template not found')
+    raise ActionController::RoutingError, 'Template not found'
   end
 
   def show
