0b19b82b9d65af8d57a5bf24bd33145124d601e7
stianste <stiansteinbakken94@gmail.com>
2018-11-08 20:42:45 +0100

    Add working endpoint for updating event
    Example mutation:
    ```
    mutation updateEventMutation($eventAttributes: EventAttributes!){
      updateEvent(eventAttributes: $eventAttributes) {
        event {
              id
              youtubeLink
        }
        success
      }
    }
    ```
    
    with the following example query variable
    
    ```
    {
      "eventAttributes" : {
          "id" : 353,
          "nonBilligTitleNo": "2",
          "titleEn" : "New test event",
          "ageLimit" : "eighteen",
          "youtubeLink" : "www.somuch.this.com"
      }
    }
    ```

--- app/graphql/mutations/update_event.rb
+++ app/graphql/mutations/update_event.rb
@@ -1,30 +1,26 @@
 # frozen_string_literal: true
 
 class Mutations::UpdateEvent < Mutations::BaseMutation
-  argument :event, Types::Inputs::EventAttributes, required: true
+  null true
+  argument :event_attributes, Types::Inputs::EventAttributes, required: true
 
-  field :event, Types::EventType, null: true
+  field :event, Types::EventType, null: false
   field :success, Boolean, null: false
 
-  def resolve(event:)
-    e = Event.find_by(id: event.id)
-    if e
-      if e.update(event.to_h)
-        {
-          event: e,
-          success: true
-        }
-      else
-        {
-          event: nil,
-          success: false
-        }
-      end
-    else
+  def resolve(event_attributes:)
+    event = Event.find_by(id: event_attributes.id)
+    puts 'LOOK HERE: attributes'
+    # if !event || !event.update(event.column_names, event.column_values)
+    puts event_attributes.to_h
+    if !event || !event.update(event_attributes.to_h)
       {
         event: nil,
         success: false
       }
     end
+    {
+      event: event,
+      success: true
+    }
   end
 end
