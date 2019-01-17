289f99270d1a7542524549adf3943cd6cac72b1f
Eirik Vale Aase <e.v.aase@gmail.com>
2019-01-17 18:39:17 +0100

    Remove extra path component

--- spec/factories/image_factory.rb
+++ spec/factories/image_factory.rb
@@ -6,7 +6,7 @@ FactoryGirl.define do
   factory :image do
     title 'Tittel'
     image_file do
-      fixture_file_upload Rails.root.join('app', 'assets', 'images', 'banner-images', 'kitteh.jpeg', 'image/png')
+      fixture_file_upload Rails.root.join('app', 'assets', 'images', 'banner-images', 'kitteh.jpeg')
     end
   end
 end
