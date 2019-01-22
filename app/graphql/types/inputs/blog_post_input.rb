bb467b78d58171b91782c2fef0ca5ac825678846
Alf <alfiehub@users.noreply.github.com>
2019-01-22 19:55:57 +0100

    GraphQL: some default values, BlogInputType & PageType & JsonType (#385)
    * default values for language arguments, BlogInputType & PageType
    
    * JsonType & errors
    
    * some more

---
+++ app/graphql/types/inputs/blog_post_input.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Types
+  module Inputs
+    class BlogPostInput < Types::Bases::BaseInputObject
+      argument :title_no, String, required: true do
+        description 'Norwegian title'
+      end
+
+      argument :title_en, String, required: true do
+        description 'English title'
+      end
+
+      argument :content_no, String, required: true
+      argument :content_en, String, required: true
+
+      argument :lead_paragraph_no, String, required: true
+      argument :lead_paragraph_en, String, required: true
+
+      argument :author_id, Integer, required: true
+
+      argument :published, Boolean, required: false, default_value: false
+      argument :publish_at, GraphQL::Types::ISO8601DateTime, required: true
+
+      argument :image_id, Integer, required: false
+    end
+  end
+end
