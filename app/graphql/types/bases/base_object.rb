f8fe79ba95aedebebe77319fa63ac14ab2c83f98
alfiehub <alfiehub@users.noreply.github.com>
2019-01-23 18:57:09 +0100

    fix rubocop

--- app/graphql/types/bases/base_object.rb
+++ app/graphql/types/bases/base_object.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class CustomConnection < GraphQL::Types::Relay::BaseConnection
-  field :total_count, Integer, "The total count of nodes.", null: false
+  field :total_count, Integer, 'The total count of nodes.', null: false
 
   def total_count
     object.nodes.size
