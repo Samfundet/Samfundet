50300d05553aad091341d3c2651a35144be0c050
Kevin Kristiansen <kevikris@gmail.com>
2019-02-14 21:13:46 +0100

    Satisfy travis

--- app/controllers/sulten/reservations_controller.rb
+++ app/controllers/sulten/reservations_controller.rb
@@ -22,7 +22,7 @@ class Sulten::ReservationsController < ApplicationController
   end
 
   def new
-    @closed_periods = Sulten::ClosedPeriod.current_and_future_closed_times.sort_by &:closed_from
+    @closed_periods = Sulten::ClosedPeriod.current_and_future_closed_times.sort_by &(:closed_from)
     @reservation = Sulten::Reservation.new
   end
 
@@ -31,7 +31,7 @@ class Sulten::ReservationsController < ApplicationController
   end
 
   def create
-    @closed_periods = Sulten::ClosedPeriod.current_and_future_closed_times.sort_by &:closed_from
+    @closed_periods = Sulten::ClosedPeriod.current_and_future_closed_times.sort_by &(:closed_from)
     @reservation = Sulten::Reservation.new(reservation_params)
     if @reservation.is_in_closed_period?
       flash.now[:error] = t('helpers.models.sulten.reservation.errors.in_closed_period')
