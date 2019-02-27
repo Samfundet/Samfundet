988121871e26512bcce3897203c07fdab8680767
Alf <alfiehub@users.noreply.github.com>
2019-02-27 10:09:44 +0100

    Use correct reservation method @ Sulten::Reservations#create (#407)

--- app/controllers/sulten/reservations_controller.rb
+++ app/controllers/sulten/reservations_controller.rb
@@ -33,7 +33,7 @@ class Sulten::ReservationsController < Sulten::BaseController
   def create
     @closed_periods = Sulten::ClosedPeriod.current_and_future_closed_times.sort_by(&:closed_from)
     @reservation = Sulten::Reservation.new(reservation_params)
-    if @reservation.is_in_closed_period?
+    if @reservation.in_closed_period?
       flash.now[:error] = t('helpers.models.sulten.reservation.errors.in_closed_period')
       render :new
     elsif @reservation.save
