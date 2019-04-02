eb3421041f31fa6c96b463dd41777039365137b4
Kevin Kristiansen <kevikris@gmail.com>
2019-04-02 12:02:13 +0200

    Use correct controller for sulten closed periods

--- app/controllers/sulten/closed_periods_controller.rb
+++ app/controllers/sulten/closed_periods_controller.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Sulten::ClosedPeriodsController < ApplicationController
+class Sulten::ClosedPeriodsController < Sulten::BaseController
   load_and_authorize_resource
 
   def index
