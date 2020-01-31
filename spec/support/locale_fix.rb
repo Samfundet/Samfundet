b7c9cd9f00a9707aee53afefee95e0e4acb63493
Eirik Vale Aase <e.v.aase@gmail.com>
2020-01-31 23:13:48 +0100

    Enforce class combine definitions

--- spec/support/locale_fix.rb
+++ spec/support/locale_fix.rb
@@ -1,18 +1,16 @@
 # frozen_string_literal: true
 
-module FixLocalesSpecs
-  module ::ActionController::TestCase::Behavior
-    alias process_without_logging process
+module FixLocalesSpecs::ActionController::TestCase::Behavior
+  alias process_without_logging process
 
-    def process(action, method = 'SOMETHING', **args)
-      if args.key?(:params)
-        args[:params][:locale] = 'en'
-      else
-        args[:params] = { locale: 'en' }
-      end
-
-      args[:method] = method if method != 'SOMETHING'
-      process_without_logging(action, **args)
+  def process(action, method = 'SOMETHING', **args)
+    if args.key?(:params)
+      args[:params][:locale] = 'en'
+    else
+      args[:params] = { locale: 'en' }
     end
+
+    args[:method] = method if method != 'SOMETHING'
+    process_without_logging(action, **args)
   end
 end
