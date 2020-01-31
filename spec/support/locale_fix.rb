b03b3e5f1675db4740d44b7cfd737fd509487776
Eirik Vale Aase <e.v.aase@gmail.com>
2020-01-31 23:47:45 +0100

    Exclude locale_fix since it broke tests

--- spec/support/locale_fix.rb
+++ spec/support/locale_fix.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
-module FixLocalesSpecs::ActionController::TestCase::Behavior
-  alias process_without_logging process
+module FixLocalesSpecs
+  module ::ActionController::TestCase::Behavior
+    alias process_without_logging process
 
-  def process(action, method = 'SOMETHING', **args)
-    if args.key?(:params)
-      args[:params][:locale] = 'en'
-    else
-      args[:params] = { locale: 'en' }
+    def process(action, method = 'SOMETHING', **args)
+      if args.key?(:params)
+        args[:params][:locale] = 'en'
+      else
+        args[:params] = { locale: 'en' }
+      end
+
+      args[:method] = method if method != 'SOMETHING'
+      process_without_logging(action, **args)
     end
-
-    args[:method] = method if method != 'SOMETHING'
-    process_without_logging(action, **args)
   end
 end
