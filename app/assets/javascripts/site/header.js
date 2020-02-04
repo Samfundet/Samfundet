eb89018aa6fe02da85829a09038580f6c637df3b
sigve <me@evgiz.net>
2020-02-04 19:50:47 +0100

    Wip redesign before branch out navbar

--- app/assets/javascripts/site/header.js
+++ app/assets/javascripts/site/header.js
@@ -1,19 +1,7 @@
 $(function() {
-  $(".dropdown-button.opening-hours").parent().on('click', function(event) {
-    $(".dropdown-content-wrapper.opening-hours").toggleClass('max-height');
-    $(".dropdown-button.opening-hours .caret-wrapper").toggleClass('flip');
-    event.preventDefault();
-  });
-
-  $(".dropdown-button.menu-items").parent().on('click', function(event) {
-    $(".dropdown-content-wrapper.menu-items").toggleClass('max-height');
-    $(".dropdown-button.menu-items .caret-wrapper").toggleClass('flip');
-    event.preventDefault();
-  });
-
-  $(".titlebar-search-link").click(function(e){
-    e.preventDefault();
-    $(".top-bar-search").toggle();
-    $(".top-bar-search #search_query").focus();
-  })
+    $("#header-menu-button-mobile").on('click', function(event) {
+        $("#header-menu-button-mobile").toggleClass('open');
+        $("#header-menu-mobile").toggleClass('open');
+        event.preventDefault();
+    });
 });
