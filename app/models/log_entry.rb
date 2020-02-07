4c47fd4aae5a46eacd415dcbcf7557f1d8b94911
Eirik Vale Aase <e.v.aase@gmail.com>
2020-02-07 01:05:30 +0100

    Show and log unlogged applicants

--- app/models/log_entry.rb
+++ app/models/log_entry.rb
@@ -9,6 +9,30 @@ class LogEntry < ApplicationRecord
   belongs_to :member
 
   default_scope { order(created_at: :asc) }
+
+  def self.possible_log_entries
+    if I18n.locale == :no
+      [
+        'Forsøkt ringt, tok ikke telefonen',
+        'Ringt og tilbudt verv, venter på svar',
+        'Ringt, venter fremdeles på svar',
+        'Ringt og tilbudt verv, takket ja',
+        'Ringt og tilbudt verv, takket nei',
+        'Ringt og meddelt ingen tilbud om verv',
+        'Sendt e-post og meddelt ingen tilbud om verv'
+      ]
+    elsif I18n.locale == :en
+      [
+        'Called, no reply',
+        'Called and offered position, awaiting reply',
+        'Called, still waiting for reply',
+        'Called and offered position, the applicant accepted',
+        'Called and offered position, the applicant declined',
+        'Called and notified the applicant of our rejection',
+        'Sent email and notified the applicant of our rejection'
+      ]
+    end
+  end
 end
 
 # == Schema Information
