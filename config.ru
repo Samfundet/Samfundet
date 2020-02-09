74073675401819926f628d71592c6a6029e760fa
Eirik Vale Aase <e.v.aase@gmail.com>
2020-02-09 12:53:01 +0100

    Configure LiveReload properly

--- config.ru
+++ config.ru
@@ -3,5 +3,7 @@
 # This file is used by Rack-based servers to start the application.
 
 require_relative 'config/environment'
+require 'rack-livereload'
 
 run Rails.application
+use Rack::LiveReload
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,6 +14,8 @@ Samfundet::Application.configure do
   # Show full error reports and disable caching
   config.consider_all_requests_local = true
 
+  config.assets.digest = false
+
   # Enable/disable caching. By default caching is disabled.
   if Rails.root.join('tmp/caching-dev.txt').exist?
     config.action_controller.perform_caching = true
@@ -52,4 +54,6 @@ Samfundet::Application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 end
