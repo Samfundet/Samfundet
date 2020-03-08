64d4619d23925aaf1a3567fe036631fa52008a6a
Tollef Jørgensen <tollefj@gmail.com>
2020-03-08 14:51:38 +0100

    updated flashes for leader election

--- app/controllers/site_controller.rb
+++ app/controllers/site_controller.rb
@@ -18,19 +18,34 @@ private
     #   %m - Month, e.g. 8 for August, 1 for January
     #   %d - Day in month
     #   %H - Hour of day
-    flash_in_date_range = lambda { |date_from, date_to, date_type, msg|
-      time_now = Time.now.strftime(date_type).to_i
+    flash_in_date_range = lambda { |date_from, date_to, msg, flash_type|
+      time_now = Date.new()
       return unless time_now.between?(date_from, date_to)
       flash[:notice] = msg
     }
+
+    valid_date = lambda {|from, to| Time.zone.today.between?(from, to)}
 
     start_msg = t('site.index.sit_samf_series1')
     link_text = t('site.index.sit_samf_series2')
     end_msg = t('site.index.sit_samf_series3')
     url = 'https://www.youtube.com/watch?v=YRnxoMCZwI8'
-    external_url = "<a target=_'blank' rel='noopener noreferrer' href=#{url}>#{link_text}</a>"
+    external_url = "<br><br><a target=_'blank' rel='noopener noreferrer' href=#{url}>#{link_text}</a>"
 
-    message = "#{start_msg} #{external_url} #{end_msg}".html_safe
-    flash_in_date_range.call(3, 5, '%m', message)
+    message = "#{start_msg} #{external_url} #{end_msg}"
+    # message = Rails::Html::LinkSanitizer.new.sanitize(raw_html_message)
+    # message = ApplicationController::Base.helpers.sanitize(raw_html)
+
+    sit_start = Date.new(2020, 3, 6)
+    sit_end = Date.new(2020, 4, 1)
+    if valid_date.call(sit_start, sit_end)
+      flash[:notice] = message.html_safe
+    end
+
+    ledervalg_start = Date.new(2020, 3, 6)
+    ledervalg_slutt = Date.new(2020, 3, 10)
+    if valid_date.call(ledervalg_start, ledervalg_slutt)
+      flash[:message] = t("site.index.election")
+    end
   end
 end
