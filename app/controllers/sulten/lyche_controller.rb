a54190d68a2e5269d18d5ded68d0c8d94e86c9a4
sigve <me@evgiz.net>
2020-03-26 19:23:20 +0100

    Lyche 'pusser opp' front page

---
+++ app/controllers/sulten/lyche_controller.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+# Temporary controller for lyche 'pusser opp' page
+class Sulten::LycheController < Sulten::BaseController
+  skip_authorization_check
+  layout "lyche"
+
+  def pusser_opp
+
+  end
+end
