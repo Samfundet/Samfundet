636b3ed2fd89abe91fc157928b007ed2cb46a76a
sigve <me@evgiz.net>
2020-08-07 12:32:48 +0200

    Add lyche header js

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module Samfundet
     config.assets.precompile += %w(job_applications/admissions_admin_job_applications.js)
     config.assets.precompile += %w(sulten/duration.js)
     config.assets.precompile += %w(sulten/calendar.js)
+    config.assets.precompile += %w(lyche/header.js)
 
     # Load local env variables into rails config
     config.before_configuration do
