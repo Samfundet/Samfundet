4fae5ba0a1968aeb5070211d537c91c02fee8fd1
sigve <me@evgiz.net>
2020-08-07 12:38:47 +0200

    Fix header path

--- config/application.rb
+++ config/application.rb
@@ -59,7 +59,7 @@ module Samfundet
     config.assets.precompile += %w(job_applications/admissions_admin_job_applications.js)
     config.assets.precompile += %w(sulten/duration.js)
     config.assets.precompile += %w(sulten/calendar.js)
-    config.assets.precompile += %w(lyche/header.js)
+    config.assets.precompile += %w(sulten/header.js)
 
     # Load local env variables into rails config
     config.before_configuration do
