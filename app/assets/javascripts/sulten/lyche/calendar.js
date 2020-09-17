34794c3cb40e94cbb14ba4b2ac1bb9ef2cd25db6
Hege <hegeaal@stud.ntnu.no>
2020-09-17 18:40:53 +0200

    changes

--- app/assets/javascripts/sulten/lyche/calendar.js
+++ app/assets/javascripts/sulten/lyche/calendar.js
@@ -1,16 +1,14 @@
 /* Datepicker for lyche reservation form */
 
 $(function() {
-    console.log("hello yellow")
-   $('#lyche-datepicker').datepicker({
-       dateFormat: 'dd-mm-yy',
-       onSelect: function(dateText) {
-           var path = window.location.href
-           if (path.indexOf("?") > -1){
-               path = path.substr(0, path.indexOf("?"))
-           }
-           window.location.href = path + "?date=" + dateText;
-
-       }
-   })
+    $('#lyche-datepicker').datepicker({
+        dateFormat: 'dd-mm-yy',
+        onSelect: function(dateText) {
+            var path = window.location.href
+            if(path.indexOf("?") > -1){
+                path = path.substr(0, path.indexOf("?"))
+            }
+            window.location.href = path + "?date=" + dateText;
+        }
+    });
 });
