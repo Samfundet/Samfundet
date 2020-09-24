7b6341529a36cb58b8683c5a6d471f6ebcd335b1
sigve <me@evgiz.net>
2020-09-24 19:58:24 +0200

    Fixes cache error for ticket purchases

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -212,17 +212,21 @@ class EventsController < ApplicationController
 
     if payment_error.blank? # Error case no. 1: Database error.
       flash[:error] = t('events.purchase_generic_error')
-      redirect_to root_path
+      payment_error_price_group = BilligPaymentErrorPriceGroup.where(error: params[:bsession]).first
+      if payment_error_price_group.present? # Error case no. 3. Field errors.
+        event = payment_error_price_group.samfundet_event
+        redirect_to buy_event_path(event, bsession: params[:bsession])
+      else
+        flash[:error] = payment_error.message
+        redirect_to root_path(bsession: params[:bsession])
+      end
     else
-      payment_error_price_group =
-        BilligPaymentErrorPriceGroup.where(error: params[:bsession]).first
-
+      payment_error_price_group = BilligPaymentErrorPriceGroup.where(error: params[:bsession]).first
       if payment_error_price_group.present? # Error case no. 3. Field errors.
         event = payment_error_price_group.samfundet_event
         redirect_to buy_event_path(event, bsession: params[:bsession])
       else # Error case no. 2. Show payment error without purchase form.
-        flash[:error] = payment_error.message
-        redirect_to root_path
+        redirect_to root_path(bsession: params[:bsession])
       end
     end
   end
