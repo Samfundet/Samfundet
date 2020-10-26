7c444f187b118fcf5c75584cf0d997888d40b4fc
Erling Van De Weijer <erlinvan@samfundet.no>
2020-10-26 20:47:05 +0100

    Point staging purchase URL to production backend, to make a test purchase

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -53,7 +53,7 @@ Samfundet::Application.configure do
     arguments: '-i'
   }
 
-  config.billig_path = 'https://billettsalg-test.uka.no/pay'
+  config.billig_path = 'https://billettsalg.samfundet.no/pay'
   config.billig_ticket_path = 'https://billig.samfundet.no/pdf?'
 
   config.after_initialize do
