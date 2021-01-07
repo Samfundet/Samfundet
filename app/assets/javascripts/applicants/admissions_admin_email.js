80f83300de24e04087b5904b4535f0c9a85032b2
sigve <me@evgiz.net>
2021-01-07 15:57:49 +0100

    Change name to <name> template in email preview

--- app/assets/javascripts/applicants/admissions_admin_email.js
+++ app/assets/javascripts/applicants/admissions_admin_email.js
@@ -3,7 +3,7 @@ $(function() {
         var intro = $('input[name="introduction"]').val()
         var subj = $('input[name="subject"]').val()
         $("#preview-subject").text(subj)
-        $("#preview-intro").text(intro+" Pernille,")
+        $("#preview-intro").text(intro+" <fornavn>,")
     });
     $('textarea').keyup(function (event) {
         var content = $('textarea[name="content"]').val()
