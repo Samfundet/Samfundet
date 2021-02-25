b57124c97926672ceed16ae6393912f8906270b1
Marcus Frenje <marcusfrenje25@gmail.com>
2021-02-25 20:31:40 +0100

    fix typos

--- setup-linux.sh
+++ setup-linux.sh
@@ -7,5 +7,4 @@ sudo apt-get install software-properties-common
 sudo apt-add-repository -y ppa:rael-gc/rvm
 sudo apt-get update
 sudo apt-get install rvm
-sudo usermod -a -G rvm user
-sudo reboot
+sudo usermod -a -G rvm $user
