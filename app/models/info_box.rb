f3419332d339217dc07b0447cb634b291d870143
snorrekr <snorre.ssk@gmail.com>
2021-03-09 21:18:03 +0100

    added color choosing possibility

--- app/models/info_box.rb
+++ app/models/info_box.rb
@@ -1,9 +1,10 @@
 class InfoBox < ApplicationRecord
+  COLOR = %w[blue red white green].freeze
 
   extend LocalizedFields
   localized_fields :title, :body, :link
 
-  validates :title_no, :title_en, :body_no, :body_en, presence: true
+  validates :title_no, :title_en, :body_no, :body_en, :bg_color, presence: true
 
   belongs_to :image
   def image_or_default
--- db/seeds/info_box.seeds.rb
+++ db/seeds/info_box.seeds.rb
@@ -1,12 +1,15 @@
 puts "Creating info box"
 
+COLOR = %w[blue red white green]
+
 3.times do
     InfoBox.create!(
       title_no: Faker::Lorem.sentence(1),
       title_en: Faker::Lorem.sentence(1),
       body_no: "les mer her",
       body_en: "read more here",
-      image_id: Image.all.sample.id
+      image_id: Image.all.sample.id,
+      bg_color: COLOR.sample
     )
 end
 puts "Done creating info box"
