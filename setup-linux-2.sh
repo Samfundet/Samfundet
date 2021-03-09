e0bfa7ca0242ce801941dd1f0d899a47d6235e55
Marcus Frenje <marcusfrenje25@gmail.com>
2021-03-09 19:05:55 +0100

    small change

--- setup-linux-2.sh
+++ setup-linux-2.sh
@@ -1,8 +1,9 @@
 sudo apt-get update
 sudo apt-get upgrade
+source ~/.rvmrc
 rvm user gemsets
 rvm install ruby
-rvm install 2.5.5 --movable
+rvm install ruby-2.5.5
 rvm use 2.5.5 --default
 sudo apt-get install postgresql postgresql-contrib libpq-dev
 echo -e "CREATE USER samfundet WITH PASSWORD 'samfundet';\nALTER USER samfundet CREATEDB;" | sudo -u postgres psql
