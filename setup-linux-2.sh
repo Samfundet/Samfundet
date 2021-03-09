0cc8556e0af568238a0f7530d5888f804321e560
Marcus Frenje <marcusfrenje25@gmail.com>
2021-03-09 21:08:05 +0100

    update

--- setup-linux-2.sh
+++ setup-linux-2.sh
@@ -1,15 +1,25 @@
+#Check for updates
 sudo apt-get update
 sudo apt-get upgrade
-source ~/.rvmrc
+
+#Install ruby
 rvm user gemsets
 rvm install ruby
 rvm install 2.5.5
-rvm use 2.5.5 --default
+
+#Install database requirements
 sudo apt-get install postgresql postgresql-contrib libpq-dev
 echo -e "CREATE USER samfundet WITH PASSWORD 'samfundet';\nALTER USER samfundet CREATEDB;" | sudo -u postgres psql
-rvm reinstall 2.5.5
+
+#Install the bundler
+gem install bundler
+gem install bundler:1.17.3
+rvm use 2.5.5 --default
+
+#Install required ruby gems
 bundle install
 
+#Copy and paste the config files, and rename them
 if [ ! -e $CONFIG_DIR/database.yml ] &&
    [ ! -e $CONFIG_DIR/local_env.yml ] &&
    [ ! -e $CONFIG_DIR/billig.yml ] &&
@@ -17,6 +27,8 @@ if [ ! -e $CONFIG_DIR/database.yml ] &&
      make copy-config-files || exit
 fi
 
+#For setting up the database for the first time
 bundle exec rails db:setup
 
+#Run the server
 make run
