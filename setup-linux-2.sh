a2ae89eea29187ca817023319c231406b9782a53
Marcus Frenje <marcusfrenje25@gmail.com>
2021-03-10 09:53:25 +0100

    test

--- setup-linux-2.sh
+++ setup-linux-2.sh
@@ -2,6 +2,12 @@
 sudo apt-get update
 sudo apt-get upgrade
 
+#Install ruby
+rvm user gemsets
+rvm install ruby
+rvm install 2.5.5
+rvm use 2.5.5 --default
+
 #Install database requirements
 sudo apt-get install postgresql postgresql-contrib libpq-dev
 echo -e "CREATE USER samfundet WITH PASSWORD 'samfundet';\nALTER USER samfundet CREATEDB;" | sudo -u postgres psql
@@ -23,6 +29,7 @@ if [ ! -e $CONFIG_DIR/database.yml ] &&
 fi
 
 #For setting up the database for the first time
+rvm use 2.5.5 --default
 bundle exec rails db:setup
 
 #Run the server
