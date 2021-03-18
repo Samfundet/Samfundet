24db7f4b79885376096fba1346f28edf806b112b
snorrekr <snorre.ssk@gmail.com>
2021-03-18 21:56:25 +0100

    fixed routes and schema

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,13 @@ Rails.application.routes.draw do
     post 'contact', to: 'contact#create'
 
     get 'search', to: 'search#search'
+
+    resources :info_boxes
+
     #resources :search, only: [:new, :create, :search]
     ############################
     ##  Routes for events     ##
     ############################
-
     resources :events do
       get :buy, on: :member, action: "buy"
       post :buy, on: :member, action: "buy"
