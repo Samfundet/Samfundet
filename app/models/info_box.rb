b14124d36b0678686fd6aa13915adc0608672e4a
snorrekr <snorre.ssk@gmail.com>
2021-03-18 21:23:33 +0100

    added translations with yml

--- app/models/info_box.rb
+++ app/models/info_box.rb
@@ -7,11 +7,16 @@ class InfoBox < ApplicationRecord
   validates :title_no, :title_en, :body_no, :body_en, :color, presence: true
 
   belongs_to :image
+
   def image_or_default
     if image.present?
       image.image_file
     else
       Image.default_image.image_file
     end
+  end
+
+  def to_s
+    title
   end
 end
