43cd9f77143fd584bf8b44ce28008e59bcf1a644
sigve <me@evgiz.net>
2021-04-13 21:00:11 +0200

    Move all to new migration

--- db/migrate/20210413184932_add_infobox_position.rb
+++
@@ -1,6 +0,0 @@
-class AddInfoboxPosition < ActiveRecord::Migration[5.2]
-  def change
-    add_column :info_boxes, :position, :integer
-    add_column :info_boxes, :image_state, :boolean
-  end
-end
--- db/migrate/20210413185908_add_infobox.rb
+++ db/migrate/20210413185908_add_infobox.rb
@@ -1,4 +1,4 @@
-class CreateInfoBoxes < ActiveRecord::Migration[5.2]
+class AddInfobox < ActiveRecord::Migration[5.2]
   def change
     create_table :info_boxes do |t|
       t.string :title_no
@@ -8,10 +8,12 @@ class CreateInfoBoxes < ActiveRecord::Migration[5.2]
       t.string :link_no
       t.string :link_en
       t.integer :image_id
+      t.boolean :image_state
       t.string :color
       t.datetime :start_time
       t.datetime :end_time
       t.timestamps
+      t.integer :position
     end
   end
 end
