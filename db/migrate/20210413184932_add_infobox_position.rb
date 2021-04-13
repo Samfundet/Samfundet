e7dea922d7135b138c858e1dccb32a081d7c0dbb
sigve <me@evgiz.net>
2021-04-13 20:55:33 +0200

    Move imagestate to 2nd migration

--- db/migrate/20210413184932_add_infobox_position.rb
+++ db/migrate/20210413184932_add_infobox_position.rb
@@ -1,5 +1,6 @@
 class AddInfoboxPosition < ActiveRecord::Migration[5.2]
   def change
     add_column :info_boxes, :position, :integer
+    add_column :info_boxes, :image_state, :boolean
   end
 end
