0c499d6456334e5a21ab1c002d51b2df404eb6b3
evgiz <evgiz@users.noreply.github.com>
2021-04-20 20:29:48 +0200

    Add some wise words and center control panel applets (#801)
    * Add some wise words and center control panel applets
    
    * Add some wise words and center control panel applets
    
    * Fix applet layout and add three wise words

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -9,6 +9,40 @@ class MembersController < ApplicationController
   has_control_panel_applet :access_applet,
                            if: -> { true }
 
+  @@wise_words = [
+      "En reke om dagen er godt for magen.",
+      "Hiv, og hoi! Snart er skatten vår!",
+      "Langemann lurer alle, som en ulv i fåreklær!",
+      "Vi jobber best når det dundrer og smeller rundt skuta vår!",
+      "Kan du danse som en reke?",
+      "Bli med til rekeland!",
+      "Alle sjødyr er velkommen i rekeland!",
+      "Lykken er et saftig rekesmørbrød.",
+      "Bli med ut for å reke!",
+      "Som du roper i skogen får du svar!",
+      "Ekte reker lever på land.",
+      "Har du noen gang sett en reke svømme 3000 meter?",
+      "Reker trenger alltid ny rogn.",
+      "Noen reker har aldri på seg t-skjorte.",
+      "Reker elsker alle dyr i sjøen! <3",
+      "Si hei til en reke!",
+      "Hvilket sjødyr er du? En krabbe kanskje?",
+      "Hvilket sjødyr er du? En blekksprut kanskje?",
+      "Store fisker spiser aldri små fisker.",
+      "Hopp i havet!",
+      "Selv om sjødyr vokser opp til slutt blir de aldri glemt.",
+      "En sprellende fisk burde få creds for å i hvertfall prøve å danse.",
+      "Ville ha en reke som kjæledyr, men reker er frie dyr.",
+      "Ikke alle pensjonister har skjegg faktisk.",
+      "Adopter en reke i dag!",
+      "Hvordan bruker egentlig en reke et tegnebrett?",
+      "Reker ser alltid på film på søndager.",
+      "Blub, blub!",
+      "Jeg husker enda da jeg var rogn.",
+      "Reker er flinke til å skrive!",
+      "Reker er sjøens influencere."
+  ]
+
   def search
     @members = Member.where(
       "UPPER(fornavn) || ' ' || UPPER(etternavn) LIKE UPPER(?)" \
@@ -32,6 +66,7 @@ class MembersController < ApplicationController
 
   def control_panel
     @applets = ControlPanel.applets(request).select(&:relevant?)
+    @wise_words = @@wise_words.sample
   end
 
   def steal_identity_applet; end
@@ -43,4 +78,5 @@ class MembersController < ApplicationController
   end
 
   def access_applet; end
+
 end
