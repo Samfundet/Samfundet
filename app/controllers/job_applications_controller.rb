6ec89fbab6d8f18367bf8ad5f6e38e2bb94e6f16
konstahm <konstantin@live.no>
2021-06-25 18:11:54 +0200

    Fix applicant login when no admission (#730)
    * Fix applicant login when no admission
    
    * rubocop fixes
    
    * Added dots, and added "associated" in the english translation
    
    Co-authored-by: konstahm <konstahm@gmail.com>
    Co-authored-by: Marcus Frenje <marcusfrenje25@gmail.com>

--- app/controllers/job_applications_controller.rb
+++ app/controllers/job_applications_controller.rb
@@ -7,6 +7,10 @@ class JobApplicationsController < ApplicationController
 
   def index
     @admissions = @current_user.job_applications.where(withdrawn: false).group_by { |job_application| job_application.job.admission }
+    raise 'Exception'
+  rescue
+    flash[:error] = t('job_applications.application_not_found')
+    redirect_to root_path
   end
 
   def create
