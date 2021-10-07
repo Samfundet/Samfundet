983642573c48e3d2c89bb7b86c2ae140b63632b0
Johanndy <johanne.dybevik@gmail.com>
2021-10-07 18:38:08 +0200

    lagt til js fil for grid/list view

--- app/assets/javascripts/events/grid-view.js
+++ app/assets/javascripts/events/grid-view.js
@@ -1,9 +1,8 @@
 
-$('#gridbutton').on('click',function(e) {
-    if($(this).hasClass('list')) {
-        $('#container ul').removeClass('grid').addClass('list');
-    }
-    else if ($(this).hasClass('grid')) {
-        $('#container ul').removeClass('list').addClass('grid');
-    }
+$(function() {
+    $('#event-grid-button').on('click', function (e) {
+        console.log("hi")
+        $('#list-event-view').remove()
+    })
 });
+
