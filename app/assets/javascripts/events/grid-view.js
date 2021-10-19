59268bb4e5f071fb17dcb8cd532373e61b394cea
Johanndy <johanne.dybevik@gmail.com>
2021-10-19 21:04:27 +0200

    fixed boxes in grid view and added icon for grid buttion

--- app/assets/javascripts/events/grid-view.js
+++ app/assets/javascripts/events/grid-view.js
@@ -1,12 +1,10 @@
 $(function() {
     $('#event-grid-button').on('click', function (e) {
-        console.log("hi")
         $('#list-event-view').hide()
         $('#grid-event-view').show()
     });
 
     $('#event-list-button').on('click', function (e) {
-        console.log("hi")
         $('#grid-event-view').hide()
         $('#list-event-view').show()
     });
