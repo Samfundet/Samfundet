2bc7d77d2d5ff396e867503cc57d66322276dee0
JohanneD <johanne.dybevik@gmail.com>
2021-10-26 23:13:47 +0200

    fikset mer på grid view, lagt inn bokser med bilder slik som forsiden

--- app/assets/javascripts/events/grid-view.js
+++ app/assets/javascripts/events/grid-view.js
@@ -1,11 +1,15 @@
 $(function() {
     $('#event-grid-button').on('click', function (e) {
         $('#list-event-view').hide()
+        $('#event-grid-button').hide()
         $('#grid-event-view').show()
+        $('#event-list-button').show()
     });
 
     $('#event-list-button').on('click', function (e) {
         $('#grid-event-view').hide()
+        $('#event-list-button').hide()
         $('#list-event-view').show()
+        $('#event-grid-button').show()
     });
 });
