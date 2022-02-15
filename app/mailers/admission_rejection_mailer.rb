723cc78099f25748e91dbac5b7a24128d3333e93
evgiz <evgiz@users.noreply.github.com>
2022-02-15 21:14:25 +0100

    Remove no-reply senders (#880)

--- app/mailers/admission_rejection_mailer.rb
+++ app/mailers/admission_rejection_mailer.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class AdmissionRejectionMailer < ActionMailer::Base
-  default from: 'no-reply@samfundet.no',
+  default from: 'opptaksansvarlig@samfundet.no',
           reply_to: 'opptaksansvarlig@samfundet.no'
 
   def send_rejection_email(applicant, template)
