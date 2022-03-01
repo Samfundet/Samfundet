4338e46784c73f163e228934ff8530be313f9ed8
Snorre <snorre.ssk@gmail.com>
2022-03-01 12:08:30 +0100

    fixed buttons for adding old tags

--- app/assets/javascripts/job_applications/prev_tags.js
+++ app/assets/javascripts/job_applications/prev_tags.js
@@ -6,13 +6,18 @@ $(function () {
     })
     $(".prev-tag-button").click(function () {
         let value = this.innerHTML
-        if (!temp.includes(value) && field.val() != "") {
-            field.val(temp + ", " +value)
+        if (!temp.includes(value)) {
+            if (field.val() == "" || field.val().slice(-2) == ", ") {
+                field.val(temp + value)
+            } else if (field.val().slice(-1) == ","){
+                field.val(temp + " " + value)
+            } else {
+                field.val(temp + ", " + value)
+            }
             temp = field.val()
-        } else if (!temp.includes(value) ){
-            field.val(temp + value)
+
+        } else {
             temp = field.val()
         }
     })
-    temp = field.val()
 })
