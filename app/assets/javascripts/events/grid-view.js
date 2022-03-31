7067d4e82adc2b15c0bc0afe270c062ab72fad79
JohanneD <johanne.dybevik@gmail.com>
2022-03-31 18:43:12 +0200

    removed list/gridview when search

--- app/assets/javascripts/events/grid-view.js
+++ app/assets/javascripts/events/grid-view.js
@@ -1,3 +1,10 @@
+
+const searchSelected = $('#search-event')
+const searchValue = searchSelected.val();
+const dropdowntype = $("option:selected", "#search-event1").text();
+const dropdownarea = $("option:selected", "#search-event2").text();
+
+
 $(function() {
     $('#event-grid-button').on('click', function (e) {
         $('.list-event-view').hide();
@@ -12,4 +19,45 @@ $(function() {
         $('.list-event-view').show();
         $('#event-grid-button').show();
     });
+
+    $("#search-event").on('change', function (e) {
+        let optionSelected = $(this)
+        let value = optionSelected.text
+        console.log(searchValue)
+        console.log(optionSelected)
+        if (value!==searchValue){
+            $('#event-list-button').hide();
+            $('#event-grid-button').hide();
+        }
+        else {
+            $('#event-list-button').hide();
+            $('#event-grid-button').show();
+        }
+    });
+
+    $("#search-event1").on('change', function (e) {
+        let optionSelected = $("option:selected", this).text();
+        if (optionSelected!==dropdowntype){
+            $('#event-list-button').hide();
+            $('#event-grid-button').hide();
+        }
+        else {
+            $('#event-list-button').hide();
+            $('#event-grid-button').show();
+        }
+    });
+
+    $("#search-event2").on('change', function (e) {
+        let optionSelected = $("option:selected", this).text();
+        if (optionSelected!==dropdownarea){
+            $('#event-list-button').hide();
+            $('#event-grid-button').hide();
+        }
+        else {
+            $('#event-list-button').hide();
+            $('#event-grid-button').show();
+        }
+    });
 });
+
+
