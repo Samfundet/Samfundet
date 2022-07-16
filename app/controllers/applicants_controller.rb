f74f739e5a663a8b644e0c94400419785e969122
Marcus Frenje <marcusfrenje@gmail.com>
2022-07-17 01:19:30 +0200

    Add translations to success message upon applicant update

--- app/controllers/applicants_controller.rb
+++ app/controllers/applicants_controller.rb
@@ -63,7 +63,7 @@ class ApplicantsController < ApplicationController
         flash[:success] = t('applicants.update_success')
         redirect_to job_applications_path
       elsif current_user.roles.include? Role.super_user
-        flash[:success] = "Søkerinformasjon endret"
+        flash[:success] = t('applicants.forms.admin.update_success')
         redirect_to members_control_panel_path
       end
     else
