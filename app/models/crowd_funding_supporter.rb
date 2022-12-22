f906a495440a50c9e1906f3f40906a31868a910f
Snorre Skjellestad Kristiansen <snorre.ssk@gmail.com>
2022-12-23 00:37:52 +0100

    Fix linting

--- app/models/crowd_funding_supporter.rb
+++ app/models/crowd_funding_supporter.rb
@@ -1,9 +1,6 @@
 # frozen_string_literal: true
 
 class CrowdFundingSupporter < ApplicationRecord
-
   enum supporter_type: %i[group student_union], _suffix: true
-
   validates :name, :amount, :supporter_type, presence: true
-
 end
