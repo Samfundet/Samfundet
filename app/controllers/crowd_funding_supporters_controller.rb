93832e4f133450a3ac3ee97e11683877c8777aa4
Snorre Skjellestad Kristiansen <snorre.ssk@gmail.com>
2022-12-23 21:29:14 +0100

    change order and add standing

--- app/controllers/crowd_funding_supporters_controller.rb
+++ app/controllers/crowd_funding_supporters_controller.rb
@@ -7,7 +7,10 @@ class CrowdFundingSupportersController < ApplicationController
                            if: -> { can? :manage, CrowdFundingSupporter }
   def index
     @supporters_union = CrowdFundingSupporter.limit(5).order('amount desc').where(supporter_type: :student_union)
-    @supporters_group = CrowdFundingSupporter.order('amount').where(supporter_type: :group)
+    @supporters_group = CrowdFundingSupporter.order('amount desc').where(supporter_type: :group)
+
+    @supporter_group_per = CrowdFundingSupporter.where(supporter_type: :group).sort { |a, b| (a.amount / a.donors) <=> (b.amount / b.donors) }.reverse
+
     @largest = CrowdFundingSupporter.order('amount').last()
     @points = [0, @largest.amount/4, @largest.amount/2, (@largest.amount*3)/4, @largest.amount]
   end
