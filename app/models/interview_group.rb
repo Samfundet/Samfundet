3507f45e19bc973990b2f1c347dc4ceba80c8457
Marcus Frenje <marcusfrenje@gmail.com>
2022-12-23 22:37:30 +0100

    Model and views

---
+++ app/models/interview_group.rb
@@ -0,0 +1,2 @@
+class InterviewGroup < ApplicationRecord
+end
---
+++ spec/models/interview_group_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe InterviewGroup, type: :model do
+  pending "add some examples to (or delete) #{__FILE__}"
+end
