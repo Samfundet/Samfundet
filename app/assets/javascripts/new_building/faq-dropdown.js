4152c4081a3c6d8578a159ba636eff4d499aff4f
Snorre Skjellestad Kristiansen <snorre.ssk@gmail.com>
2023-03-03 17:50:21 +0100

    fix seed and remove console.log

--- app/assets/javascripts/new_building/faq-dropdown.js
+++ app/assets/javascripts/new_building/faq-dropdown.js
@@ -16,5 +16,3 @@ for (i = 0; i < acc.length; i++) {
         }
     });
 }
-
-console.log("hoi")
--- db/seeds/merch.seeds.rb
+++ db/seeds/merch.seeds.rb
@@ -1,7 +1,7 @@
 puts "Create Merch"
 
-puts "Create Product"
-5.times do
+puts "Create Product with Variation"
+2.times do
   Product.create!(
     name_no: Faker::GameOfThrones.character,
     name_en: Faker::GameOfThrones.character,
@@ -12,11 +12,23 @@ end
 
 SIZES = %w[XS S M L XL]
 puts "create Product variations"
-Product.all.each do |product|
+Product.limit(2).each do |product|
   2.times do
     product.product_variations.create!(
       specification: SIZES[rand(SIZES.length)],
       quantity: rand(100)
     )
   end
+end
+
+puts "Create Product without Variation"
+2.times do
+  Product.create!(
+    name_no: Faker::GameOfThrones.character,
+    name_en: Faker::GameOfThrones.character,
+    price: rand(100),
+    image_id: Image.offset(1).limit(1).sample.id,
+    has_variations: false,
+    amount: rand(100)
+  )
 end
