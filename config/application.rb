4a5949bf12b68394dff1b2881c930cb8213be2fa
robines <espi.robin@gmail.com>
2024-02-09 00:33:36 +0100

    Add www-bookworm to config.hosts

--- config/application.rb
+++ config/application.rb
@@ -18,6 +18,7 @@ module Samfundet
     config.hosts << "samfundet.no"
     config.hosts << "navi.samfundet.no"
     config.hosts << "www-beta.samfundet.no"
+    config.hosts << "www-bookworm.samfundet.no"
 
     # Only load the plugins named here, in the order given (default is alphabetical).
     # :all can be used as a placeholder for all plugins not explicitly named.
