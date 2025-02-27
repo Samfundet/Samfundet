754552a475fe105d83059058c6fe166a743f8715
Snorre Skjellestad Kristiansen <54811445+snorrekr@users.noreply.github.com>
2025-02-27 18:44:44 +0100

    Add venues dropdown to navbar (#1127)
    * add new dropdown to menu
    
    * fix bogey wogey
    
    * fix bogey wogey... again nininin🙃

--- app/assets/javascripts/site/header.js
+++ app/assets/javascripts/site/header.js
@@ -13,7 +13,18 @@ $(function() {
     $("#info-button").removeClass('enabled');
     $("#info-drawer").addClass('hidden');
     $(".info-drawer-hide-other").removeClass('hidden');
+    $("#venues-button").removeClass('enabled');
+    $("#venues-drawer").addClass('hidden');
+    $(".venues-drawer-hide-other").removeClass('hidden');
     event.preventDefault();
   });
+
+  $("#venues-button").on('click', function(event) {
+    $("#venues-button").toggleClass('enabled');
+    $("#venues-drawer").toggleClass('hidden');
+    $(".venues-drawer-hide-other").toggleClass('hidden');
+    event.preventDefault();
+  });
+
 
 });
