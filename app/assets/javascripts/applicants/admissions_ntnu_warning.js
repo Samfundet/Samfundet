cf73a7bf831fcac3a14193ffcd68b6b7747ac544
Robin <16273164+robines@users.noreply.github.com>
2025-08-16 16:35:30 +0200

    Disable registering as applicant with Microsoft-owned email (#1135)
    * Tmp: Disable applicant registration with Microsoft-owned emails
    
    * Suggest student mail as possible alternative email provider
    
    * Revert "Suggest student mail as possible alternative email provider"
    
    This reverts commit d6a9691d11fa0b19e38859a8f57140b487012f2a.
    
    * rubocop
    
    * single quotes

--- app/assets/javascripts/applicants/admissions_ntnu_warning.js
+++ app/assets/javascripts/applicants/admissions_ntnu_warning.js
@@ -1,9 +1,13 @@
-$(function() {
+$(function () {
     $("#applicant_email").on("input", function (event) {
-        if($("#applicant_email")[0].value.toString().includes("@ntnu.no")) {
+        const email = $("#applicant_email")[0].value.toString().toLowerCase();
+        if (email.includes("@ntnu.no")) {
             $(".ntnu_warning").css("display", "block");
+        } else if (email.includes("@microsoft.") || email.includes("@live.") || email.includes("@outlook.") || email.includes("@hotmail.")) {
+            $(".microsoft_warning").css("display", "block");
         } else {
             $(".ntnu_warning").css("display", "none");
+            $(".microsoft_warning").css("display", "none");
         }
     });
 });
